d6e9423d7357fdd453b09351bbb195fd1f01b6c1
fizz <2545789628@qq.com>
2023-03-04 14:42:47 +0800

    git-third-hotfix-ts

--- src/main/java/fizz/branchs.java
+++ src/main/java/fizz/branchs.java
@@ -3,7 +3,7 @@ package fizz;
 public class branchs {
 
     public static void main(String[] args) {
-        System.out.println("这是hot-fix分支正常合并测试");
+        System.out.println("这是hot-fix分支冲突合并测试");
 
 
     }
