b108e6cea842fbbd9112baaa54e9e215f37ebd6c
FishFizz <126648656+FishFizz@users.noreply.github.com>
2023-03-04 15:31:17 +0800

    pull ts

--- src/main/java/fizz/branchs.java
+++ src/main/java/fizz/branchs.java
@@ -6,6 +6,9 @@ public class branchs {
         System.out.println("这是master分支冲突合并测试");
         System.out.println("这是hot-fix分支冲突合并测试");
         System.out.println("这是推送测试");
+        System.out.println("这是拉取远程库代码测试");
+
+        
 
 
     }
